97e1d3363f3b8af44dfda2f5b64ae9be1565c9f0
Diego Ulises <51557342+IDiegoUlises@users.noreply.github.com>
2020-04-17 18:27:35 -0400

    Update Modificar-Variable.ino

--- Arduino/Modificar-Variable.ino
+++ Arduino/Modificar-Variable.ino
@@ -1,2 +1,25 @@
-```c++
-variable 
+
+void setup()
+{
+ //Inicia el puerto serial
+ Serial.begin(9600);
+  
+//Crea una variable
+int variable = 3;
+
+//Crea un puntero
+int *puntero; 
+
+//Puntero a punta a una direccion de memoria
+puntero = &variable;
+
+//El puntero  cambia el valor de una variable selecionada
+*puntero = 8; 
+ 
+//Muestra la variable
+ Serial.println(variable);
+}
+
+void loop()
+{
+}
